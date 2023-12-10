c958f8990f0a19462bdd505aef9ee97234dc3d7a
Insidious Beaver <insidiousbeaver@proton.me>
2023-12-10 11:11:48 +0100

    done

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,17 @@ struct Race {
 fn main() {
     let input_path = env::var("aoc_2023_06_path").unwrap() + "/input.txt";
     let input = fs::read_to_string(input_path).unwrap();
-    let races = parse_input(input);
+    let races = parse_input(&input);
     let mut part1_result = 1;
     for r in races {
         let res = calculate_race(r);
         part1_result *= res.len();
     }
-    println!("{}", part1_result);
+    println!("part1 result: {}", part1_result);
+    let races_2 = parse_input_2(&input);
+    let mut part2_result = 1;
+    part2_result *= calculate_race(races_2[0]).len();
+    println!("part2 result: {}", part2_result);
 }
 fn calculate_race(r: Race) -> Vec<u64> {
     let mut res = vec![];
@@ -27,7 +31,7 @@ fn calculate_race(r: Race) -> Vec<u64> {
 
     return res;
 }
-fn parse_input(input: String) -> Vec<Race> {
+fn parse_input(input: &String) -> Vec<Race> {
     let mut parsed_input: Vec<Race> = vec![];
     let mut times: Vec<u64> = vec![];
     let mut distances: Vec<u64> = vec![];
@@ -69,5 +73,38 @@ fn parse_input(input: String) -> Vec<Race> {
         });
     }
 
+    return parsed_input;
+}
+
+fn parse_input_2(input: &String) -> Vec<Race> {
+    let mut parsed_input: Vec<Race> = vec![];
+    let mut times: Vec<u64> = vec![];
+    let mut distances: Vec<u64> = vec![];
+    for (i, line) in input.lines().enumerate() {
+        let mut word = String::new();
+        for (j, c) in line.char_indices().skip(10) {
+            if c.is_ascii_digit() {
+                word.push(c);
+                if j == line.len() - 1 {
+                    if i == 0 {
+                        let a = u64::from_str_radix(&word, 10).unwrap();
+                        times.push(a);
+                        word.clear();
+                    }
+                    if i == 1 {
+                        let a = u64::from_str_radix(&word, 10).unwrap();
+                        distances.push(a);
+                        word.clear();
+                    }
+                }
+            }
+        }
+    }
+    for i in 0..times.len() {
+        parsed_input.push(Race {
+            time: times[i],
+            distance: distances[i],
+        });
+    }
     return parsed_input;
 }
